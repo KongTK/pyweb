f0fa1df6259c4ea7d6c2d5a42c5dd20045df1dd8
KongTK <kongtk@naver.com>
2021-10-20 09:57:34 +0900

    git ignore

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z_t5mwtg4scys9vmg21(v#i@2=1$bpo=@e17igh^#rjar24z+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
